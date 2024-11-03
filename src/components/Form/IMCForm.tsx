b7bd9b309066263798bc798c35d1cf98711e9eae
Fábio Santos <fabiotrabmat@gmail.com>
2024-11-03 13:15:22 -0300

    fix: corrigindo o não envio do formulário se os inputs contiverem valores inválidos.

--- src/components/Form/IMCForm.tsx
+++ src/components/Form/IMCForm.tsx
@@ -1,68 +1,88 @@
-import { useState } from "react";
+import {useState} from "react";
 import Label from "./Label";
 import NumberInput from "./NumberInput";
 import savingLastValuesInLocalStorage from "../../utils/savingLastValuesInLocalStorage";
 
 type Props = {
-	imcParameters: { height: string; weight: string };
-	handleIMCValues(parameters: { height: string; weight: string }): void;
-	handleScrollToResult(): void;
+    imcParameters: { height: string; weight: string };
+    handleIMCValues(parameters: { height: string; weight: string }): void;
+    handleScrollToResult(): void;
 };
 
-export default function IMCForm({ imcParameters, handleIMCValues, handleScrollToResult }: Props) {
-	const [heightValue, setHeightValue] = useState(imcParameters.height);
-	const [weightValue, setWeightValue] = useState(imcParameters.weight);
+export default function IMCForm({imcParameters, handleIMCValues, handleScrollToResult}: Props) {
+    const [heightValue, setHeightValue] = useState(imcParameters.height);
+    const [weightValue, setWeightValue] = useState(imcParameters.weight);
+    const [isValidInputs, setIsValidInputs] = useState(true);
 
-	function handleToCalculateButtonClick(event: React.MouseEvent) {
-		event.preventDefault();
-		const imc = {
-			height: heightValue,
-			weight: weightValue,
-		};
+    function handleToCalculateButtonClick(event: React.MouseEvent) {
+        event.preventDefault();
 
-		handleIMCValues(imc);
+        if (heightValue === "0" || heightValue === "") {
+            setIsValidInputs(false);
+            return
+        }
 
-		setTimeout(() => {
-			handleScrollToResult();
-			savingLastValuesInLocalStorage(imc);
-		}, 180);
-	}
+        if (weightValue === "0" || weightValue === "") {
+            setIsValidInputs(false);
+            return
+        }
 
-	return (
-		<>
-			<form className="w-3/4 max-w-2xl p-5 my-8 ">
-				<div className="flex flex-col items-center h-full rounded-[52px] dark:bg-neutral-800/70 justify-evenly bg-neutral-500/20">
-					<div className="flex flex-col items-center w-full">
-						<Label
-							labelText="Entre com sua altura em centímetros (cm):"
-							inputForElementForLabel="height"
-						/>
-						<NumberInput
-							initialInputValue={imcParameters.height}
-							sendingInputValue={setHeightValue}
-							imcParameter="height"
-							labelForPointingThisInput="height"
-						/>
-					</div>
-					<div className="flex flex-col items-center w-full">
-						<Label
-							labelText="Entre com seu peso em quilogramas (kg):"
-							inputForElementForLabel="weight"
-						/>
-						<NumberInput
-							initialInputValue={imcParameters.weight}
-							sendingInputValue={setWeightValue}
-							imcParameter="weight"
-							labelForPointingThisInput="weight"
-						/>
-					</div>
-					<button
-						onClick={handleToCalculateButtonClick}
-						className="w-4/5 px-8 py-4 text-2xl font-semibold rounded-full bg-lime-400 sm:w-1/2 dark:bg-violet-900 dark:hover:bg-violet-950 hover:bg-lime-500/80">
-						Calcular
-					</button>
-				</div>
-			</form>
-		</>
-	);
+        setIsValidInputs(true);
+
+        const imc = {
+            height: heightValue,
+            weight: weightValue,
+        };
+
+        handleIMCValues(imc);
+
+        setTimeout(() => {
+            handleScrollToResult();
+            savingLastValuesInLocalStorage(imc);
+        }, 180);
+    }
+
+    return (
+        <>
+            <form className="w-3/4 max-w-2xl p-5 my-8 " >
+                <div className="flex flex-col items-center h-full rounded-[52px] dark:bg-neutral-800/70 justify-evenly bg-neutral-500/20" >
+                    <div className="flex flex-col items-center w-full" >
+                        <Label
+                            labelText="Entre com sua altura em centímetros (cm):"
+                            inputForElementForLabel="height"
+                        />
+                        <NumberInput
+                            initialInputValue={imcParameters.height}
+                            sendingInputValue={setHeightValue}
+                            imcParameter="height"
+                            labelForPointingThisInput="height"
+                            isValidInputs={isValidInputs}
+                            handleOnChange={setIsValidInputs}
+                        />
+                    </div >
+                    <div className="flex flex-col items-center w-full" >
+                        <Label
+                            labelText="Entre com seu peso em quilogramas (kg):"
+                            inputForElementForLabel="weight"
+                        />
+                        <NumberInput
+                            initialInputValue={imcParameters.weight}
+                            sendingInputValue={setWeightValue}
+                            imcParameter="weight"
+                            labelForPointingThisInput="weight"
+                            isValidInputs={isValidInputs}
+                            handleOnChange={setIsValidInputs}
+                        />
+                    </div >
+                    <button
+                        onClick={handleToCalculateButtonClick}
+                        disabled={!isValidInputs}
+                        className=
+                            "w-4/5 px-8 py-4 text-2xl font-semibold rounded-full bg-lime-400 sm:w-1/2 dark:bg-violet-900 dark:hover:bg-violet-950 hover:bg-lime-500/80" >
+                        Calcular
+                    </button >
+                </div >
+            </form >
+        </>
+    );
 }
--- src/components/Form/NumberInput.tsx
+++ src/components/Form/NumberInput.tsx
@@ -5,6 +5,8 @@ type Props = {
 	imcParameter: string;
 	labelForPointingThisInput: string;
 	sendingInputValue(value: string): void;
+	isValidInputs: boolean,
+	handleOnChange(value: boolean): void;
 };
 
 export default function NumberInput({
@@ -12,11 +14,20 @@ export default function NumberInput({
 	imcParameter,
 	labelForPointingThisInput,
 	sendingInputValue,
+	isValidInputs,
+	handleOnChange
 }: Props): React.ReactNode {
 	const [inputValue, setInputValue] = useState(initialInputValue);
 
 	function handleGettingInputValue(event: React.ChangeEvent) {
 		const inputElement = event.target as HTMLInputElement & string;
+		if (inputElement.value === "0" || inputElement.value === "") {
+			setInputValue(inputElement.value);
+			handleOnChange(false);
+			return;
+		}
+		handleOnChange(true);
+
 		let inputChangedValue: string = inputElement.value;
 
 		if (inputElement.value.endsWith(",")) {
@@ -34,7 +45,9 @@ export default function NumberInput({
 		<>
 			<input
 				onChange={handleGettingInputValue}
-				className="w-3/4 px-4 py-3 text-lg text-center sm:text-xl text-zinc-600 sm:4/5"
+				className={`w-3/4 px-4 py-3 text-lg text-center sm:text-xl text-zinc-600 sm:4/5 ${
+					!isValidInputs ? "border-2 border-red-600 dark:border-red-400" : ""
+				}`}
 				type="text"
 				value={inputValue}
 				name={imcParameter}
@@ -42,6 +55,11 @@ export default function NumberInput({
 				placeholder={inputValue === "" ? "Digite seu valor..." : ""}
 				required
 				/>
+			{!isValidInputs && (
+				<span className="text-lg text-red-700 italic w-3/4 text-center mt-1 dark:text-red-400">
+					Confira seus valores. Entre somente com números, não deixe o campo vazio ou digite 0.
+				</span>
+			)}
 		</>
 	);
 }
